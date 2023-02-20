68e9efe57eff980b0678316f5290f5de4e18c2cb
yoavelmalem <yoav.el@certora.com>
2023-02-20 17:01:05 +0200

    git test

--- certoratomation/__main__.py
+++ certoratomation/__main__.py
@@ -3,4 +3,3 @@ from .cli import run_server
 
 if __name__ == '__main__':
     run_server()
-    # certoratomation.run_server()
--- certoratomation/cli.py
+++ certoratomation/cli.py
@@ -13,9 +13,9 @@ def cli():
 
 
 @click.command()
+@click.option("-ra", "--run-all", is_flag=True, help="Run Server As Part Of CI")
 @click.option("-h", "--host", default=None, help="Server Host")
 @click.option("-p", "--port", default=None, help="Server Port")
-def run_server(host: str = None, port: int = None):
+def run_server(host: str = None, port: int = None, run_all: bool = False):
     from .server.app import run_server
     run_server(host, port)
-    pass
